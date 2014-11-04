fff14fccd5c84f978e32ab6a293abbadf25753d8
vigo2013 <fw121fw4@163.com>
2014-11-04 17:40:05 +0800

    增加页面意见反馈的QQ

--- src/main/java/com/examw/test/front/model/record/UserPaperRecordInfo.java
+++ src/main/java/com/examw/test/front/model/record/UserPaperRecordInfo.java
@@ -18,7 +18,7 @@ import com.examw.support.CustomDateSerializer;
 @JsonSerialize(include = Inclusion.NON_NULL)
 public class UserPaperRecordInfo extends Paging {
 	private static final long serialVersionUID = 1L;
-	private String id,userId,paperId,productId,paperName,paperTypeName,subjectId;
+	private String id,userId,userName,paperId,productId,paperName,paperTypeName,subjectId;
 	private Integer status,terminalCode,paperType,rightNum;
 	private Long usedTime;
 	private BigDecimal score;
@@ -270,6 +270,22 @@ public class UserPaperRecordInfo extends Paging {
 	 */
 	public void setRightNum(Integer rightNum) {
 		this.rightNum = rightNum;
+	}
+	/**
+	 * 获取 用户名
+	 * @return userName
+	 * 用户名
+	 */
+	public String getUserName() {
+		return userName;
+	}
+	/**
+	 * 设置 用户名
+	 * @param userName
+	 * 用户名
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
 	}
 	
 }
